641ca3a1ea53c1101ebd38397893c0f0563add1d
Diana <diana.amfreitas@gmail.com>
2020-03-01 08:59:08 +0000

    p02_04

--- practical/p02/p02_04b.c
+++ practical/p02/p02_04b.c
@@ -7,14 +7,19 @@
 #include <string.h>
 
 
-#define MAX_NAME_SIZE   30
+#define MAX_NAME_SIZE   50
 #define MAX_GRADE_SIZE  2
 #define MAX_STUDENTS    20
 
-int main(int argc, char *argv[]){
-   char grade[MAX_GRADE_SIZE+1];
+struct student{
+   int grade;
    char name[MAX_NAME_SIZE+1];
+};
+
+int main(int argc, char *argv[]){
    int fd;
+   struct student s;
+   char grade[MAX_GRADE_SIZE+1];
 
    if(argc != 2){
       printf("Usage: %s <destination>\n",argv[0]);
@@ -31,17 +36,17 @@ int main(int argc, char *argv[]){
 
    while(i < MAX_STUDENTS){
 
-      memset(name,0,sizeof(name));
+      memset(s.name,0,sizeof(s.name));
       memset(grade,0,sizeof(grade));
 
       printf("Student %d: ",i+1);
-      fgets(name,MAX_NAME_SIZE+1,stdin);
+      fgets(s.name,MAX_NAME_SIZE+1,stdin);
 
-      if (name[strlen(name)-1] == '\n')
-         name[strlen(name)-1] = '\0';
+      if (s.name[strlen(s.name)-1] == '\n')
+         s.name[strlen(s.name)-1] = '\0';
       else getchar();
 
-      if(name[0] == '\0') break;
+      if(s.name[0] == '\0') break;
 
       printf("Grade %d: ", i+1);
       fgets(grade,MAX_GRADE_SIZE+1,stdin);
@@ -50,28 +55,13 @@ int main(int argc, char *argv[]){
          grade[strlen(grade)-1] = '\0';
       }else getchar();
 
-      if(write(fd, name, strlen(name))!=strlen(name)){
-         perror(argv[1]);
-         close(fd);
-         exit(3);
-      }
-      if(write(fd, " ", 1)!=1){
-         perror(argv[1]);
-         close(fd);
-         exit(3);
-      }
-      if(write(fd, grade, strlen(grade))!=strlen(grade)){
-         perror(argv[1]);
-         close(fd);
-         exit(3);
-      }
+      s.grade = atoi(grade);
 
-       if(write(fd, "\n", 1)!=1){
+      if(write(fd, &s, sizeof(struct student))!=sizeof(struct student)){
          perror(argv[1]);
          close(fd);
          exit(3);
       }
-
       i++;
    }
 
